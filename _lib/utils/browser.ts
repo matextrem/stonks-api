e59dcc3bb2b648852d836fe3a303395a9bc1149e
Mati Dastugue <mdastugu@amazon.com>
2024-04-16 05:35:53 +0100

    Disable sandbox

--- _lib/utils/browser.ts
+++ _lib/utils/browser.ts
@@ -6,11 +6,23 @@ export const getBrowser = async (): Promise<Browser> => {
   const isProd = process.env.NODE_ENV === 'production';
 
   browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu',
+    ],
     executablePath: isProd
       ? await chrome.executablePath()
       : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
 
     headless: true,
+    defaultViewport: chrome.defaultViewport,
+    ignoreHTTPSErrors: true,
   });
 
   return browser;
