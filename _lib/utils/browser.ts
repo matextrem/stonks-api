b0a49c904e020ba1702f489badda16049058269c
Mati Dastugue <mdastugu@amazon.com>
2024-04-16 04:56:42 +0100

    Use headless:true in order to improve performance

--- _lib/utils/browser.ts
+++ _lib/utils/browser.ts
@@ -2,24 +2,29 @@ import chrome from '@sparticuz/chromium';
 import puppeteer, { Browser } from 'puppeteer-core';
 
 export const getBrowser = async (): Promise<Browser> => {
+  let browser;
   const isProd = process.env.NODE_ENV === 'production';
 
-  let browser;
+  browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-web-security', // disabling CORS
+      '--disable-site-isolation-trials',
+      '--disable-notifications',
+      '--no-zygote',
+    ],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: isProd
+      ? await chrome.executablePath()
+      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
 
-  if (isProd) {
-    browser = await puppeteer.launch({
-      args: chrome.args,
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath(),
-      headless: 'new',
-      ignoreHTTPSErrors: true,
-    });
-  } else {
-    browser = await puppeteer.launch({
-      headless: 'new',
-      executablePath:
-        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    });
-  }
+    headless: true,
+    devtools: false,
+    slowMo: 0,
+    ignoreHTTPSErrors: true,
+  });
+
   return browser;
 };
