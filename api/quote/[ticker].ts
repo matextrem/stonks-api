876e7e6aa2aa2d946bca8f4ee12bfb2ead30de00
Mati Dastugue <mdastugu@amazon.com>
2024-04-17 03:37:48 +0100

    Remove unnecessary log

--- api/quote/[ticker].ts
+++ api/quote/[ticker].ts
@@ -3,7 +3,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { fetchStockData } from '../../_lib/utils';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  console.time('Browser Launch');
   try {
     const stockData = await fetchStockData('quote', req.query.ticker as string);
     return res.json({ ...stockData });
